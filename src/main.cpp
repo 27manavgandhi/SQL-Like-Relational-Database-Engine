758b3fed6a6001ebc748df1be4a63980a26a9a03
Manav Gandhi <27manavgandhi@gmail.com>
2026-06-27 11:14:40 +0530

    feat(cli): implement command-line execution pipeline

---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+
+#include "parser.h"
+#include "databaseManager.h"
+#include "error.h"
+
+int main(int argc, char* argv[]) {
+    std::ifstream file;
+    try {
+        if(argc < 2) throw RuntimeException(RuntimeError::FILE_NOT_PROVIDED, 0, 0, "", "");
+
+        std::string fileName = argv[1];
+        if(fileName.length() < 4 || fileName.substr(fileName.size() - 4) != ".ark") throw RuntimeException(RuntimeError::INVALID_FILE_EXTENSION, 0, 0, fileName, ".ark");
+
+        file.open(fileName);
+
+        if(!file) throw RuntimeException(RuntimeError::FILE_NOT_FOUND, 0, 0, argv[1], ""); 
+    }
+    catch(const ArkException& e) {
+        std::cerr << e.what();
+        return 1;
+    }
+
+    DatabaseManager manager;
+
+    std::string command;
+    char ch;
+    int currentLine = 1;
+    int commandStartLine = 1;
+
+    while(file.get(ch)) {
+        if(ch == '\n') currentLine++;
+
+        if(ch == '-' && file.peek() == '-') {
+            while(file.get(ch) && ch != '\n');
+            
+            currentLine++;
+            commandStartLine = currentLine;
+            continue;
+        }
+
+        command += ch;
+
+        if(ch == ';') {
+            
+            try {
+                Parser parser(command, commandStartLine);
+                parser.parse(manager); 
+            }
+            catch(const ArkException& e) {
+                std::cerr << e.what() << "\n";
+
+                return 1;
+            }
+
+            command.clear();
+
+            commandStartLine = currentLine;
+        }
+    } 
+
+    return 0;
+}
