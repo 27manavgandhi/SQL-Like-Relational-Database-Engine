39c55093f8b7a0a8ddc14eb243ac02809d16b5ca
Manav Gandhi <27manavgandhi@gmail.com>
2026-06-27 11:13:55 +0530

    feat(core): implement relational database engine data structures

---
+++ include/core.h
@@ -0,0 +1,184 @@
+#pragma once
+#ifndef CORE_H
+#define CORE_H
+
+#include <string>
+#include <vector>
+
+//====================================================================== DATATYPES ======================================================================
+
+enum class DataType {
+    NONE,
+    INT,
+    DOUBLE,
+    STRING,
+    BOOL
+};
+
+union Value {
+    int Int;
+    double Double;
+    bool Bool;
+};
+
+enum class ValidationResult { 
+    OK, 
+    COUNT_MISMATCH, 
+    TYPE_MISMATCH 
+};
+
+//====================================================================== COLUMN ======================================================================
+
+class Column {
+private :
+    std::string columnName;
+    DataType type;
+
+public :
+    Column(const std::string& name, DataType type);
+
+    std::string getName() const;
+    void setName(std::string newName);
+    DataType getType() const;
+};
+
+//================================ CELL ================================
+
+class Cell {
+private :
+    DataType type;
+    Value value;
+    std::string stringValue;
+    bool isNull;
+
+public :
+    Cell();
+    Cell(int value);
+    Cell(double value);
+    Cell(const std::string& value);
+    Cell(bool value);
+
+    DataType getType() const;
+    bool null() const;
+
+    int getInt() const;
+    double getDouble() const;
+    std::string getString() const;
+    bool getBool() const;
+
+    bool isNullValue() const;
+    bool isInt() const;
+    bool isDouble() const;
+    bool isString() const;
+    bool isBool() const;
+
+    void setInt(int value);
+    void setDouble(double value);
+    void setString(const std::string& value);
+    void setBool(bool value);
+    void setNull();
+
+    std::string toString() const;
+
+    bool operator==(const Cell& other) const;
+    bool operator!=(const Cell& other) const;
+    bool operator<(const Cell& other) const;
+    bool operator>(const Cell& other) const;
+    bool operator<=(const Cell& other) const;
+    bool operator>=(const Cell& other) const;
+};
+
+//================================ ROW ================================
+
+class Row {
+private :
+    std::vector<Cell> cells;
+public :
+    void addCell(const Cell& cell);
+
+    void setCell(int index, const Cell& cell);
+
+    Cell getCell(int index) const;
+
+    int getCellCount() const;
+
+    void dropCell(int index);
+
+    std::string toString() const;
+};
+
+//================================ ROW ================================
+
+class Table {
+private :
+    std::string tableName;
+    std::vector<Column> columns;
+    std::vector<Row> rows;
+
+    std::string structureFilePath;
+    std::string dataFilePath;
+
+    ValidationResult validateRow(const Row& row) const;
+
+public :
+    Table(const std::string& name);
+    Table(const std::string& name, const std::string& structurePath, const std::string& dataPath);
+
+    std::string getName() const;
+    void setName(std::string newName);
+
+    void addColumn(const Column& column);
+
+    Column& getColumn(int index);
+    int getColumnCount() const;
+    std::string getColumName(int index) const;
+    DataType getColumnType(int index) const;
+    void dropColumn(int index);
+
+    int getRowCount() const;
+
+    ValidationResult insertRow(const Row& row);
+
+    void updateCell(int rowIndex, int columnIndex, const Cell& newValue);
+
+    Row* getRow(int rowIndex);
+
+    void deleteRow(int rowIndex);
+    void clearRows();
+
+    std::vector<Row> selectAll() const;
+
+    void setStructurePath(const std::string& path);
+    void setDataPath(const std::string& path);
+
+    bool saveStructureToFile() const;
+    bool loadStructureFromFile();
+
+    bool saveDataToFile() const;
+    bool loadDataFromFile();
+};
+
+//====================================================================== DATABASE ======================================================================
+
+class Database {
+private :
+    std::string databaseName;
+    std::vector<Table> tables;
+    std::string databaseFolderPath;
+public :
+    Database();
+    Database(const std::string& name, const std::string& folderPath);
+    
+    std::string getName();
+    Table* getTable(const std::string& tableName);
+
+    bool createTable(const std::string& tableName);
+    void dropTable(const std::string& tableName);
+
+    std::vector<std::string> listTables() const;
+
+    void loadDatabase();
+    void saveDatabase();
+};
+
+#endif
---
+++ src/core.cpp
@@ -0,0 +1,622 @@
+#include "core.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+
+//====================================================================== COLUMN ======================================================================
+
+Column::Column(const std::string& name, DataType type) : columnName(name), type(type) {}
+
+std::string Column::getName() const {
+    return columnName;
+}
+
+void Column::setName(std::string newName) {
+    columnName = newName;
+}
+
+DataType Column::getType() const {
+    return type;
+}
+
+//====================================================================== CELL ======================================================================
+
+Cell::Cell() : type(DataType::NONE), stringValue(""), isNull(true) {
+    value.Double = 0.0;
+}
+
+Cell::Cell(int value) : type(DataType::INT) {
+    this->value.Int = value;
+    isNull = false;
+}
+
+Cell::Cell(double value) : type(DataType::DOUBLE) {
+    this->value.Double = value;
+    isNull = false;
+}
+
+Cell::Cell(const std::string& value) : type(DataType::STRING) {
+    stringValue = value;
+    isNull = false;
+}
+
+Cell::Cell(bool value) : type(DataType::BOOL) {
+    this->value.Bool = value;
+    isNull = false;
+}
+
+DataType Cell::getType() const {
+    return type;
+}
+
+int Cell::getInt() const {
+    if(isNull) return 0;
+
+    return value.Int;
+}
+
+double Cell::getDouble() const {
+    if(isNull) return 0.0;
+
+    return value.Double;
+}
+
+std::string Cell::getString() const {
+    if(isNull) return "";
+
+    return stringValue;
+}
+
+bool Cell::getBool() const {
+    if(isNull) return false;
+
+    return value.Bool;
+}
+
+bool Cell::isNullValue() const {
+    return isNull;
+}
+
+bool Cell::isInt() const {
+    return type == DataType::INT && !isNull;
+}
+
+bool Cell::isDouble() const {
+    return type == DataType::DOUBLE && !isNull;
+}
+
+bool Cell::isString() const {
+    return type == DataType::STRING && !isNull;
+}
+
+bool Cell::isBool() const {
+    return type == DataType::BOOL && !isNull;
+}
+
+void Cell::setInt(int value) {
+    this->value.Int = value;
+    isNull = false;
+    type = DataType::INT;
+}
+
+void Cell::setDouble(double value) {
+    this->value.Double = value;
+    isNull = false;
+    type = DataType::DOUBLE;
+}
+
+void Cell::setString(const std::string& value) {
+    stringValue = value;
+    isNull = false;
+    type = DataType::STRING;
+}
+
+void Cell::setBool(bool value) {
+    this->value.Bool = value;
+    isNull = false;
+    type = DataType::BOOL;
+}
+
+void Cell::setNull() {
+    isNull = true;
+    type = DataType::NONE;
+    value.Double = 0.0;
+    stringValue = "";
+}
+
+std::string Cell::toString() const {
+    if(isNull) return "NULL";
+
+    switch(type) {
+        case DataType::INT :
+            return std::to_string(value.Int);
+        case DataType::DOUBLE :
+            return std::to_string(value.Double);
+        case DataType::STRING : 
+            return stringValue;
+        case DataType::BOOL :
+            return value.Bool ? "true" : "false";
+        case DataType::NONE :
+        default : return "NONE";
+    }
+}
+
+bool Cell::operator==(const Cell& other) const {
+    if(isNull || other.isNull) return false;
+
+    if(type != other.type) {
+        return false;
+    }
+
+    switch(type) {
+        case DataType::INT :
+            return (value.Int == other.value.Int);
+        case DataType::DOUBLE :
+            return (value.Double == other.value.Double);
+        case DataType::STRING :
+            return (stringValue == other.stringValue);
+        case DataType::BOOL :
+            return (value.Bool == other.value.Bool);
+        case DataType::NONE :
+        default : return true;
+    }
+}
+
+bool Cell::operator!=(const Cell& other) const {
+    return !(*this == other);
+}
+
+bool Cell::operator<(const Cell& other) const {
+    if(isNull || other.isNull) return false;
+
+    if(type != other.type) {
+        return (type < other.type);
+    }
+
+    switch(type) {
+        case DataType::INT :
+            return (value.Int < other.value.Int);
+        case DataType::DOUBLE :
+            return (value.Double < other.value.Double);
+        case DataType::STRING :
+            return (stringValue < other.stringValue);
+        case DataType::BOOL :
+            return (value.Bool < other.value.Bool);
+        case DataType::NONE :
+        default : return false;
+    }
+}
+
+bool Cell::operator>(const Cell& other) const {
+    return (other < *this);
+}
+
+bool Cell::operator<=(const Cell& other) const {
+    return !(other < *this);
+}
+
+bool Cell::operator>=(const Cell& other) const {
+    return !(*this < other);
+}
+
+//====================================================================== ROW ======================================================================
+
+void Row::addCell(const Cell& cell) {
+    cells.push_back(cell);
+}
+
+void Row::setCell(int index, const Cell& cell){
+    cells[index] = cell;
+}
+
+Cell Row::getCell(int index) const {
+    return cells[index];
+}
+
+int Row::getCellCount() const {
+    return static_cast<int>(cells.size());
+}
+
+void Row::dropCell(int index) {
+    cells.erase(cells.begin() + index);
+}
+
+std::string Row::toString() const {
+    std::string result;
+
+    for(size_t i = 0; i < cells.size(); i++) {
+        result += cells[i].toString();
+
+        if(i != cells.size() - 1) result += " | ";
+    }
+
+    return result;
+}
+
+//====================================================================== TABLE ======================================================================
+
+Table::Table(const std::string& name) : tableName(name) {};
+Table::Table(const std::string& name, const std::string& structurePath, const std::string& dataPath) : tableName(name), structureFilePath(structurePath), dataFilePath(dataPath) {}
+
+std::string Table::getName() const {
+    return tableName;
+}
+
+void Table::setName(std::string newName) {
+    tableName = newName;
+}
+
+void Table::addColumn(const Column& column) {
+    columns.push_back(column);   
+}
+
+Column& Table::getColumn(int index) {
+    return columns.at(index);
+}
+
+int Table::getColumnCount() const {
+    return static_cast<int>(columns.size());
+}
+
+std::string Table::getColumName(int index) const {
+    return columns.at(index).getName();
+}
+
+DataType Table::getColumnType(int index) const {
+    return columns.at(index).getType();
+}
+
+void Table::dropColumn(int index) {
+    columns.erase(columns.begin() + index);
+}
+
+int Table::getRowCount() const {
+    return static_cast<int>(rows.size());
+}
+
+ValidationResult Table::validateRow(const Row& row) const {
+    if(row.getCellCount() != static_cast<int>(columns.size())) return ValidationResult::COUNT_MISMATCH;
+
+    for(size_t i = 0; i < columns.size(); i++) {
+        const Cell& cell = row.getCell(i);
+
+        if(cell.isNullValue()) continue;
+
+        if(cell.getType() != columns.at(i).getType()) return ValidationResult::TYPE_MISMATCH;
+    }
+
+    return ValidationResult::OK;
+}
+
+ValidationResult Table::insertRow(const Row& row) {
+    ValidationResult result = validateRow(row);
+    if (result != ValidationResult::OK) return result;
+
+    rows.push_back(row);
+    return ValidationResult::OK;
+}
+
+Row* Table::getRow(int rowIndex) {
+    if(rowIndex >= 0 && rowIndex < rows.size()) return &rows.at(rowIndex);
+
+    return nullptr;
+}
+
+void Table::deleteRow(int rowIndex) {
+    if(rowIndex < 0 || rowIndex >= static_cast<int>(rows.size())) {
+        std::cout << "Invalid row Index\n";
+        return;
+    }
+
+    rows.erase(rows.begin() + rowIndex);
+}
+
+void Table::clearRows() {
+    rows.clear();
+}
+
+std::vector<Row> Table::selectAll() const {
+    return rows;
+}
+
+void Table::updateCell(int rowIndex, int columnIndex, const Cell& newValue) {
+    if(rowIndex < 0 || rowIndex >= static_cast<int>(rows.size())) {
+        std::cerr << "Invalid Row Index.\n";
+        return;
+    }
+
+    if(columnIndex < 0 || columnIndex >= static_cast<int>(columns.size())) {
+        std::cerr << "Invalid Column Index.\n";
+        return;
+    }
+
+    if(newValue.getType() != columns.at(columnIndex).getType()) {
+        std::cerr << "Type mismatch. Update Failed.\n";
+        return;
+    }
+
+    rows.at(rowIndex).setCell(columnIndex, newValue);
+
+    std::cout << "Cell update successfully.\n";
+}
+
+void Table::setStructurePath(const std::string& path) {
+    structureFilePath = path;
+}
+
+void Table::setDataPath(const std::string& path) {
+    dataFilePath = path;
+}
+
+bool Table::saveStructureToFile() const {
+    std::ofstream fout(structureFilePath);
+    if(!fout) {
+        std::cerr << "Failed to open structure file.\n";
+        return false;
+    }
+
+    fout << tableName << "\n";
+
+    for(const auto& col : columns) {
+        fout << col.getName() << " ";
+
+        switch(col.getType()) {
+            case DataType::INT : fout << "INT"; break;
+            case DataType::DOUBLE : fout << "DOUBLE"; break;
+            case DataType::STRING : fout << "STRING"; break;
+            case DataType::BOOL : fout << "BOOL"; break;
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+    return true;
+}
+
+bool Table::loadStructureFromFile() {
+    std::ifstream fin(structureFilePath);
+
+    if(!fin) {
+        std::cerr << "Failed to open structure file.\n";
+        return false;
+    }
+
+    columns.clear();
+
+    std::getline(fin,tableName);
+
+    std::string colName, typeStr;
+    while(fin >> colName >> typeStr) {
+        DataType type;
+
+        if(typeStr == "INT")
+            type = DataType::INT;
+        else if(typeStr == "DOUBLE")
+            type = DataType::DOUBLE;
+        else if(typeStr == "STRING")
+            type = DataType::STRING;
+        else if(typeStr == "BOOL") 
+            type = DataType::BOOL;
+        else if(typeStr == "NULL")
+            type = DataType::NONE;
+        else {
+            std::cerr << "Unknown data type: " << typeStr << '\n';
+            return false;
+        }
+
+        columns.emplace_back(colName, type);
+    }
+
+    fin.close();
+    return true;
+}
+
+bool Table::saveDataToFile() const {
+    std::ofstream fout(dataFilePath);
+
+    if(!fout) {
+        std::cerr << "Failed to open data file.\n";
+        return false;
+    }
+
+    for(const auto& row : rows) {
+        for(size_t i = 0; i < row.getCellCount(); i++) {
+            Cell cell = row.getCell(i);
+            
+            if(cell.isString()) fout << "\"" << cell.getString() << "\"";
+            else fout << cell.toString();
+
+            if(i != row.getCellCount() - 1) fout << ",";
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+    return true;
+}
+
+bool Table::loadDataFromFile() {
+    std::ifstream fin(dataFilePath);
+
+    if(!fin) {
+        std::cerr << "Failed to open data file.\n";
+        return false;
+    }
+
+    rows.clear();
+
+    std::string line;
+    
+    while(std::getline(fin, line)) {
+        std::stringstream ss(line);
+        std::string value;
+
+        Row row;
+        int columnIndex = 0;
+
+        while(std::getline(ss, value, ',') && columnIndex < columns.size()) {
+            DataType type = columns[columnIndex].getType();
+
+            if(value == "NULL") {
+                row.addCell(Cell());
+                columnIndex++;
+                continue;
+            }
+
+            switch(type) {
+                case DataType::INT : {
+                    int intVal = std::stoi(value);
+                    row.addCell(Cell(intVal));
+                    break;
+                }
+                case DataType::DOUBLE : {
+                    double doubleVal = std::stod(value);
+                    row.addCell(Cell(doubleVal));
+                    break;
+                }
+                case DataType::STRING : {
+                    if(value.size() >= 2 && value[0] == '\"' && value[value.size() - 1] == '\"') value = value.substr(1, value.size() - 2);
+                    row.addCell(Cell(value));
+                    break;
+                }
+                case DataType::BOOL : {
+                    bool boolVal = (value == "1" || value == "true");
+                    row.addCell(Cell(boolVal));
+                    break;
+                }
+                default : break;
+            }
+
+            columnIndex++;
+        }
+
+        if(validateRow(row) == ValidationResult::OK) rows.push_back(row);
+    }
+
+    fin.close();
+    return true;
+}
+
+//====================================================================== DATABASE ======================================================================
+
+Database::Database() : databaseName(""), databaseFolderPath("") {}
+
+Database::Database(const std::string& name, const std::string& folderPath) : databaseName(name), databaseFolderPath(folderPath) {
+    std::filesystem::create_directories(databaseFolderPath + "/tables");
+    std::filesystem::create_directories(databaseFolderPath + "/data");
+}
+
+std::string Database::getName() {
+    return databaseName;
+}
+
+Table* Database::getTable(const std::string& tableName) {
+    for(Table& table : tables) {
+        if(table.getName() == tableName) {
+            return &table;
+        }
+    }
+    return nullptr;
+}
+
+bool Database::createTable(const std::string& tableName) {
+    if(getTable(tableName) != nullptr) return false;
+
+    std::string structurePath = databaseFolderPath + "/tables/" + tableName + ".tbl";
+    std::string dataPath = databaseFolderPath + "/data/" + tableName + ".dat";
+
+    if(std::filesystem::exists(structurePath) || std::filesystem::exists(dataPath)) return false;
+
+    tables.emplace_back(Table(tableName, structurePath, dataPath));
+
+    return true;
+}
+
+void Database::dropTable(const std::string& tableName) {
+    for(size_t i = 0; i < tables.size(); i++) {
+        if(tables.at(i).getName() == tableName) {
+
+            std::string structurePath = databaseFolderPath + "/tables/" + tableName + ".tbl";
+            std::string dataPath = databaseFolderPath + "/data/" + tableName + ".dat";
+
+            std::filesystem::remove(structurePath);
+            std::filesystem::remove(dataPath);
+
+            tables.erase(tables.begin() + i);
+
+            std::cout << "Table dropped: " << tableName << "\n";
+            return;
+        }
+    }
+
+    std::cerr << "Table not found.\n";
+}
+
+std::vector<std::string> Database::listTables() const {
+    std::vector<std::string> names;
+
+    for(size_t i = 0; i < tables.size(); i++) {
+        names.push_back(tables.at(i).getName());
+    }
+
+    return names;
+}
+
+void Database::saveDatabase() {
+    std::string registryPath = databaseFolderPath + "/tables.meta";
+    std::ofstream fout(registryPath);
+
+    if(!fout) {
+        std::cerr << "Failed to save tables registry.\n";
+        return;
+    }
+
+    for(size_t i = 0; i < tables.size(); i++) {
+        fout << tables.at(i).getName() << "\n"; 
+
+        tables.at(i).saveStructureToFile();
+        tables.at(i).saveDataToFile();
+    }
+
+    std::cout << "Database saved.\n";
+
+    fout.close();
+}
+
+void Database::loadDatabase() {
+    tables.clear();
+
+    std::string registryPath = databaseFolderPath + "/tables.meta";
+    
+    if(!std::filesystem::exists(registryPath)) return;
+
+    std::ifstream fin(registryPath);
+    if(!fin) {
+        std::cerr << "Failed to open table registry.\n";
+        return;
+    }
+    
+    std::string tableName;
+
+    while(std::getline(fin, tableName)) {
+        if(tableName.empty()) continue;
+
+        std::string structurePath = databaseFolderPath + "/tables/" + tableName + ".tbl";
+        std::string dataPath = databaseFolderPath + "/data/" + tableName + ".dat";
+        
+        Table table(tableName, structurePath, dataPath);
+        
+        table.loadStructureFromFile();
+        table.loadDataFromFile();
+
+        tables.push_back(table);
+    }
+
+    std::cout << "Database loaded.\n";
+
+    fin.close();
+}
